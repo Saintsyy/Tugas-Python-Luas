fbbc90d1f4202e0c9776664100e4a791e09ce38c
Saintsyy <saints.androni18@gmail.com>
2025-02-27 11:21:58 +0700

    tabung.py

--- tabung.py
+++ tabung.py
@@ -1,6 +1,6 @@
 r = int(input("Masukkan Jari - jari : "))
 t = int(input("Masukkan Tinggi : "))
 
-volume = float(3.14 * r * r * t)
+volume = 3.14 * r * r * t
 
 print(f'Hasilnya adalah : {volume:.2f}')
